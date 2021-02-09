a61e954d80a221de2bbaf7af2a8490ceb21db440
GoAl <976687036@qq.com>
2021-02-09 14:25:13 +0800

    Delete 1.py

--- data/1.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-for line in sys.stdin:
-    userid = line.strip().split('\t')[0]
-    url = "http://192.168.87.10:9999/?userid=" + userid + "&itemid=6326709127"
-    print url
